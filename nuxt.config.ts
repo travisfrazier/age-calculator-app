e52237621e7f5288d3f0b77d1fcdffbe7aac790f
Travis Frazier <Travis.Frazier@ivanti.com>
2023-12-16 16:22:36 -0800

    updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@formkit/nuxt']
+    modules: ['@formkit/nuxt'],
+    vite: {
+        define: {
+          __VUE_PROD_DEVTOOLS__: true
+        }
+      }
 })
